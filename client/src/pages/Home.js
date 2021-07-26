241ed54302806b38ea4e0f5e7df27e71a2d43cc3
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-07-25 20:28:41 -0400

    insert homepage carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import Carousel from "../components/Carousel.js";
 
 const Home = () => {
   return (
     <div>
       <Nav />
-      <h1>Homepage</h1>
+      <div className="home-header">
+        <h1>Homepage</h1>
+      </div>
+      <div className="carousel-container">
+        <Carousel name="Popular Items" />
+      </div>
+      <div className="carousel-container">
+        <Carousel name="Popular Categories" />
+      </div>
+      <div className="carousel-container">
+        <Carousel name="Popular Users" />
+      </div>
       <div className="home-container"></div>
       <Footer />
     </div>
