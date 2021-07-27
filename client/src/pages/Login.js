b11285b4a07a05c6356b39c06598ec915b4a1d4c
Dipti R <razdan.dipti@gmail.com>
2021-07-27 10:14:37 -0400

    Created the login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,6 +3,8 @@ import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
+import loginImg from "../assets/tlogo.png";
+import "./style.scss";
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -30,40 +32,50 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+    <div className="container">
+      <Link to="/signup"> Signup</Link>
+      <div className="content">
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+        <h2 className='header'>Login</h2>
+        <form onSubmit={handleFormSubmit}>
+          <div className="image">
+            <img src={loginImg} alt='dp' />
           </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+          <div className="form">
+            <div className='form-group'>
+              <label htmlFor="email">Email:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          {error ? (
+            <div>
+              <p className="error-text">The provided credentials are incorrect</p>
+            </div>
+          ) : null}
+
+          <div className="footer">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+
     </div>
   );
 }
