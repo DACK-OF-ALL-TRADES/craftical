e1e89fc1e4d78ec6889b5aaaaa50c2706ecf0889
Ashiemotto <ashheadley@gmail.com>
2021-07-28 16:15:52 -0400

    updating search page

--- client/src/components/Search/search-body.js
+++ client/src/components/Search/search-body.js
@@ -8,9 +8,33 @@ const SearchBody = ()=> {
                 <div className='home-header'>
                      <h1>Search</h1>
                 </div>
-                <div class="ui input focus">
+                <div class="ui big input focus">
                     <input type="text" placeholder="Search..."/>
                 </div>
+                <div class="ui form">
+                <div class="grouped fields">
+                <label>What would you like to search by?</label>
+                <div class="field">
+                <div class="ui radio checkbox">
+                <input type="radio" name="progress" />
+                <label>In Progress</label>
+                </div>
+                </div>
+                <div class="field">
+                <div class="ui radio checkbox">
+                <input type="radio" name="completed"/>
+                <label>Completed</label>
+                 </div>
+                </div>
+                <div class="field">
+                <div class="ui radio checkbox">
+                <input type="radio" name="example2"/>
+                <label>Future Idea</label>
+                </div>
+                </div>
+  </div>
+</div>
+              
                 
             </Container>
         </div>
