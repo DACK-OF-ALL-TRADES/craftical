ca770c0e790e9f94181297e950eb0a88274c72a2
Dipti R <razdan.dipti@gmail.com>
2021-07-29 19:52:01 -0400

    Changed to login landing

--- client/src/pages/Enter.js
+++ client/src/pages/Enter.js
@@ -1,9 +1,43 @@
-import React from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+//import React from "react";
 import Auth from "../utils/auth";
 import { Link, Redirect } from "react-router-dom";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+//import { Link } from "react-router-dom";
+import { LOGIN } from "../utils/mutations";
+
+import loginImg from "../assets/tlogo.png";
+import "./style.scss";
+
 
 const Enter = () => {
   function showNavigation() {
+    const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+    
     if (Auth.loggedIn()) {
       return (
         <div>
@@ -20,14 +54,53 @@ const Enter = () => {
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">Signup</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
+        <div className="container">
+         
+          <div className="content">
+    
+            <h2 className='header'>Login</h2>
+            <form onSubmit={handleFormSubmit}>
+              <div className="image">
+                <img src={loginImg} alt='dp' />
+              </div>
+              <div className="form">
+                <div className='form-group'>
+                  <label htmlFor="email">Email:</label>
+                  <input
+                    placeholder="email@test.com"
+                    name="email"
+                    type="email"
+                    id="email"
+                    onChange={handleChange}
+                  />
+                </div>
+    
+                <div className="form-group">
+                  <label htmlFor="pwd">Password:</label>
+                  <input
+                    placeholder="******"
+                    name="password"
+                    type="password"
+                    id="pwd"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              {error ? (
+                <div>
+                  <p className="error-text">The provided credentials are incorrect</p>
+                </div>
+              ) : null}
+    
+              <div className="footer">
+                <button type="submit">Submit</button>
+                <h4>New to Craftical?<Link to="/signup"> Signup here</Link></h4>
+              </div>
+             
+            </form>
+          </div>
+    
+        </div>
       );
     }
   }
@@ -40,7 +113,7 @@ const Enter = () => {
         </h1> */}
         <nav>{showNavigation()}</nav>
       </header>
-      <h1>Sign in / Log in Page</h1>
+      
     </div>
   );
 };
